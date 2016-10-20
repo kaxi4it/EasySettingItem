759b3d108f8a506b1816604705e01d4cf7593b37
guyj <guyj@handsure.com>
2016-10-21 04:06:17 +0800

    1.2 添加了2个回调，增加了有效的点击间隔时间，添加了间隔时间过快所需的提示框

--- app/src/main/java/com/easy/easysettingitem/MainActivity.java
+++ app/src/main/java/com/easy/easysettingitem/MainActivity.java
@@ -6,22 +6,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.guyj.EasySettingItem;
 
 public class MainActivity extends AppCompatActivity {
-    private EasySettingItem item;
+    private EasySettingItem item1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        item=(EasySettingItem)findViewById(R.id.list_item);
-        item.setOnClickListener(new View.OnClickListener() {
+        item1=(EasySettingItem)findViewById(R.id.list_item1);
+        item1.setOnItemClickListener(new EasySettingItem.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-//                ((EasySettingItem)v).getCheckBox().setChecked(! ((EasySettingItem)v).getCheckBox().isChecked());
-                ((EasySettingItem)v).getCheckBox().toggle();
-                Log.d("checkState", ((EasySettingItem)v).getCheckBox().isChecked()+"");
+            public void itemClick(EasySettingItem EasyItem) {
+                Log.i("tag",EasyItem.getCheckBox().isChecked()+"");
+            }
+        });
+        item1.setCheckBoxChangekListener(new EasySettingItem.OnCheckBoxChangeListener() {
+            @Override
+            public void checkBoxChange(CheckBox checkBox, boolean isChecked, LinearLayout EasyItem) {
+                Log.i("tag","box");
             }
         });
 
--- library/src/main/java/com/guyj/EasySettingItem.java
+++ library/src/main/java/com/guyj/EasySettingItem.java
@@ -3,38 +3,56 @@ package com.guyj;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.Checkable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * Created by kaxi4it on 2016/10/12.
  */
 public class EasySettingItem extends LinearLayout{
-    private ImageView icon;
-    private TextView text;
-    private ImageView arrow;
-    private CheckBox checkBox;
-    private LayoutParams iconParams;
-    private LayoutParams textParams;
-    private LayoutParams arrowParams;
-    private LayoutParams cbParams;
-    private int iconResID;
-    private int arrowResID;
-    private String textContent;
-    private int textColor;
-    private int textSize;
-    private int leftMargin;
-    private int topMargin;
-    private int rightMargin;
-    private int bottomMargin;
-    private boolean isChecked;
-    private int cbDrawableResID;
+    private ImageView icon;//左侧图标
+    private TextView text;//中间文字
+    private ImageView arrow;//右侧图标
+    private CheckBox checkBox;//最右侧单选框
+    private LayoutParams iconParams;//左侧图标的Params
+    private LayoutParams textParams;//文字的Params
+    private LayoutParams arrowParams;//右侧图标的Params
+    private LayoutParams cbParams;//check的Params
+    private int iconResID;//左侧图标资源ID
+    private int arrowResID;//右侧图标资源ID，一般为向右箭头
+    private String textContent;//文字内容
+    private int textColor;//文字颜色
+    private int textSize;//文字尺寸，sp/px/dp在xml中都支持
+    private int leftMargin;//文字与左侧的外边距
+    private int topMargin;//文字与上侧的外边距
+    private int rightMargin;//文字与右侧的外边距
+    private int bottomMargin;//文字与下侧的外边距
+    private boolean isChecked;//单选框的选中状态，实时更新
+    private int cbDrawableResID;//单选框的selector资源ID
+    private long intervalTime=0l;//点击区间时间，2次点击间隔过短则判为无效点击，不做响应
+    private long firstClickTime=0l;//记录第一次点击事件
+    private long nextClickTime=0l;//记录第二次点击事件
+    private Toast toast;
+    private String toastMsg;
+    private OnCheckBoxChangeListener cbListener;//含有checkBox时，可使用的listener,无checkBox时无效
+    private OnItemClickListener itemListener;//含有checkBox时，可使用的listener,无checkBox时无效
+
+    //含有checkBox时，优先返回checkbox与选中状态，其他控件也一并返回方便可能的其他操作
+    public interface OnCheckBoxChangeListener{void checkBoxChange(CheckBox checkBox,boolean isChecked,LinearLayout EasyItem);}
+    public interface OnItemClickListener{void itemClick(EasySettingItem EasyItem);}
+
+    public void setCheckBoxChangekListener(OnCheckBoxChangeListener cbListener){this.cbListener=cbListener;}
+    public void setOnItemClickListener(OnItemClickListener itemListener){this.itemListener=itemListener;}
 
     public ImageView getIcon() {
         return icon;
@@ -87,7 +105,8 @@ public class EasySettingItem extends LinearLayout{
         topMargin = typedArray.getDimensionPixelOffset(R.styleable.EasySettingItem_textMarginTop, 0);//得到的是px
         rightMargin = typedArray.getDimensionPixelOffset(R.styleable.EasySettingItem_textMarginRight, 0);//得到的是px
         bottomMargin = typedArray.getDimensionPixelOffset(R.styleable.EasySettingItem_textMarginBottom, 0);//得到的是px
-
+        intervalTime = typedArray.getInt(R.styleable.EasySettingItem_intervalTime,0);
+        toastMsg = typedArray.getString(R.styleable.EasySettingItem_toastMsg);
         typedArray.recycle();
 
     }
@@ -120,8 +139,54 @@ public class EasySettingItem extends LinearLayout{
             checkBox.setButtonDrawable(cbDrawableResID);
             cbParams=new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             addView(checkBox, cbParams);
+            this.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkBox.toggle();
+                    isChecked=checkBox.isChecked();
+                    if (cbListener!=null){
+                        cbListener.checkBoxChange(checkBox,isChecked,EasySettingItem.this);
+                    }
+                    if (itemListener!=null){
+                        itemListener.itemClick(EasySettingItem.this);
+                    }
+                }
+            });
+        }else{
+            this.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (itemListener!=null){
+                        itemListener.itemClick(EasySettingItem.this);
+                    }
+                }
+            });
         }
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                firstClickTime=System.currentTimeMillis();
+                if (firstClickTime-nextClickTime<intervalTime){
+                    if (!TextUtils.isEmpty(toastMsg)){
+                        if (toast==null) {
+                            toast = Toast.makeText(getContext(), toastMsg, Toast.LENGTH_SHORT);
+                        }else{
+                            toast.setText(toastMsg);
+                            toast.setDuration(Toast.LENGTH_SHORT);
+                        }
+                        toast.show();
+                    }
+                    this.setClickable(false);
+                }else{
+                    this.setClickable(true);
+                }
+                nextClickTime=firstClickTime;
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     private int px2sp(float pxValue) {
